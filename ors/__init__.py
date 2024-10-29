62cc725deea4f799b43bdca6b552d1124bc4d071
João Fauvel <jmmfauvel@gmail.com>
2024-10-29 13:24:39 -0300

    adding health endpoint to __all__ in __init__.py

--- ors/__init__.py
+++ ors/__init__.py
@@ -2,4 +2,4 @@ from ors.client import client, context
 from ors.endpoints import isochrones
 
 
-__all__ = ["client", "context", "isochrones"]
+__all__ = ["client", "context", "isochrones", "health"]
